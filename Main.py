0f86b502c2902e47383ddee0227d786f72591552
bhatiaharraj-droid <bhatiaharraj@gmail.com>
2026-06-27 13:58:34 +0530

    changes on variables and identifier

---
+++ Main.py
@@ -0,0 +1,24 @@
+#print statment :it is used to display the output on the screen
+
+
+print("Welcome to python")
+print(100)
+print(100+50)
+
+#start indentifiers: it is the name given to a variable,function
+#start it can start with a letter or underscore and contain numbers but cannot start with a number and also cannot contain spaces
+
+student_name="Harraj"
+marks=95
+print("the name of student is",student_name)
+print("the marks is",marks)
+
+#Variables:it is the container used to store data
+
+name="harraj"
+age=12
+print("the name of student is",name)
+print("the age of student",age)
+
+#Keywords:keywords are reserved words in python and cannot be used as variable name
+#Example if,else,for,while,True,False
